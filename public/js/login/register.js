87776d3279c54da164cd8d8de7adc7e0bce10fe2
zhchk <649844514@qq.com>
2016-12-21 14:15:37 +0800

    modified login

--- public/js/login/register.js
+++ public/js/login/register.js
@@ -1,42 +1,53 @@
-var sf = document.getElementById("sf");
-sf.onchange = function(){
-	var url =this.options[this.selectedIndex].value;
-	if (url != "") {
-		open(url,'_self');
-		this.selectedIndex=0;
-		this.blur();
-	}
-};
+var $sf = $("#sf");
+var $hid_sf = $(".hid_sf");
+var $sflis = $(".hid_sf span");
+
+$sf.on("click",function(){
+	$hid_sf.show()
+});
+$sflis .on("click",function(){
+	$sf[0].innerHTML = this.innerHTML;
+	$hid_sf.hide();
+});
+
 setInterval(test,1000);
 function test(){
-	var name=document.getElementById("names" ).value;
-	var password = document.getElementById("psd" ).value;
-	var password2 = document.getElementById("psd2" ).value;
-	var mark =  document.getElementById("mark" ).value;
-	var register = document.getElementsByClassName("register")[0];
-	var registerClass =register.getAttribute("class");
+	var name = $("#names" ).val();
+	var password = $("#psd" ).val();
+	var password2 = $("#psd2" ).val();
+	var mark =  $("#mark" ).val();
+	var register = $(".register" );
 	if(name =="" || password ==""||password2 ==""||mark ==""){
-		registerClass = registerClass.replace("active","");
-		register.setAttribute("class",registerClass );
+		register.removeClass("active")
 	}else{
-		registerClass = registerClass.concat(" active");
-		register.setAttribute("class","register active" );
-		register.onclick=function(){
-			location.href = 'register-success.html';
-		};
+		register.addClass("active");
+		var sf = $("#sf")[0].innerHTML;
+		var hid_sf = $(".hid_sf span");
+		register.on("click",function(){
+			hid_sf.each(function(){
+				var str = this.innerHTML;
+				var url = this.title;
+				if(str == sf){
+					if (url != "") {
+						open(url,'_self');
+					}
+				}
+			})
+		})
 	}
 }
 var wait=60;
 function time(o){
 	if (wait==0) {
 		o.removeAttribute("disabled");
-		o.innerHTML="输入验证码";
-		o.style.backgroundColor="#fe9900";
+		o.innerHTML="获取验证码";
+		o.style.backgroundColor="#ed692b";
 		wait=60;
 	}else{
 		o.setAttribute("disabled", true);
+
 		o.innerHTML=wait+"s后重新获取";
-		o.style.backgroundColor="#8f8b8b";
+		o.style.backgroundColor="#c7c7c7";
 		wait--;
 		setTimeout(function(){
 			time(o)

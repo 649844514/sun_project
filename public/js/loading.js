4c19ec9884e274d47ad8c2a26242ee06bcc3ebb5
zhchk <649844514@qq.com>
2016-12-23 11:53:18 +0800

    add

--- public/js/loading.js
+++ public/js/loading.js
@@ -4,3 +4,26 @@ window.onload = function(){
 	},2000)
 
 };
+/*
+$("#send").on("click",function(){
+	var username = $("#user" ).val();
+	var passwd = $("#passwd" ).val();
+
+	$.ajax({
+		url:'/api/user/login',
+		type:'POST',
+		dataType:'json',
+		data:{
+			username:username,
+			passwd:passwd
+		},
+		success:function(result){
+			console.log('result',result);
+			if(result.code==1){
+				alert("登录成功")
+			}else{
+				alert(result.msg);
+			}
+		}
+	});
+});*/
